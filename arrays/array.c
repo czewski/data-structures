50b2d9d4465740778e3c6a9fac199a767bcd2c61
eduardo <eduardohb10@hotmail.com>
2025-01-29 11:54:13 -0300

    feat: sort and index of value

--- arrays/array.c
+++ arrays/array.c
@@ -3,7 +3,10 @@
 
 // Function prototype
 int *insertInArray(int value, int *size, int *ptr);
-
+int *removeValueFromArray(int value, int *size, int *ptr);
+int getIndexOfValue(int value, int *size, int *ptr);
+void mergeSort(int arr[], int left, int right);
+void printArray(int arr[], int size);
 int main()
 {
     // address of the block created hold by this pointer
@@ -26,23 +29,38 @@ int main()
                "calloc.\n");
 
         printf("Your initial array is \n");
-        for (i = 0; i <= size; i++)
+        for (i = 0; i < size; i++)
         {
-            ptr[i] = i + 1;
+            ptr[i] = rand() % 20; // i + 1;
             printf("%d ", ptr[i]);
         }
         printf("\n");
     }
 
+    // INSERT
     int x;
     printf("Value to add: ");
     scanf("%d", &x);
-    insertInArray(x, &size, ptr);
+    ptr = insertInArray(x, &size, ptr);
+    printArray(ptr, size);
 
-    for (i = 0; i <= size; i++)
-    {
-        printf("%d ", ptr[i]);
-    }
+    // REMOVE
+    printf("\n");
+    printf("remove func \n");
+    ptr = removeValueFromArray(2, &size, ptr);
+    printArray(ptr, size);
+
+    // INDEX OF VALUE
+    printf("\n");
+    printf("return index of value %d ", x);
+    int index;
+    index = getIndexOfValue(x, &size, ptr);
+    printf("index is: %d", index);
+
+    // SORT ARRAY
+    printf("\n");
+    mergeSort(ptr, 0, size - 1);
+    printArray(ptr, size);
 
     free(ptr);
     return 0;
@@ -50,23 +68,139 @@ int main()
 
 int *insertInArray(int value, int *size, int *ptr)
 {
-    // temp to hold initial array
     int *temp = ptr;
     *size = *size + 1;
 
-    //  using realloc
     ptr = realloc(ptr, *size * sizeof(int));
     if (!ptr)
     {
-        printf("Memory Re-allocation failed.");
+        printf("realloc fail \n");
         ptr = temp;
     }
     else
     {
-        printf("Memory successfully re-allocated using "
-               "realloc.\n");
-        ptr[*size] = value;
+        printf("realloc \n");
+        ptr[*size - 1] = value;
     }
 
     return ptr;
+}
+
+int *removeValueFromArray(int value, int *size, int *ptr)
+{
+    int i;
+    int *temp = ptr;
+    for (i = 0; i < *size; i++)
+    {
+        if (ptr[i] == value)
+        {
+            int j;
+            for (j = i; j < *size - 1; j++)
+            {
+                ptr[j] = ptr[j + 1];
+            }
+            *size = *size - 1;
+            break;
+        }
+    }
+
+    ptr = realloc(ptr, *size * sizeof(int));
+    if (!ptr)
+    {
+        printf("Realloc failed.\n");
+        exit(1);
+    }
+
+    printf("Realloc succeeded after removal.\n");
+    return ptr;
+}
+
+int getIndexOfValue(int value, int *size, int *ptr)
+{
+    int index;
+    int i;
+    for (i = 0; i < *size; i++)
+    {
+        if (ptr[i] == value)
+        {
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int i, j;
+    int n1 = mid - left + 1; // Left side
+    int n2 = right - mid;    // Right side
+    // I can start an array with max size, cool
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+    {
+        L[i] = arr[left + i];
+    }
+
+    for (i = 0; i < n2; i++)
+    {
+        R[i] = arr[mid + 1 + i];
+    }
+
+    i = 0;
+    j = 0;
+    int k = left; // first index of subarray
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy remaining elements of L[], if any
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy remaining elements of R[], if any
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Recursive calls, till 1 element left
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        // Split and recurse
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 }
